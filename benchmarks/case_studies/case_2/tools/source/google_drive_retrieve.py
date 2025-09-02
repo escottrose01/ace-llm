ae9716878225e8d3f69713b81fc3cf39dbd38df3
Tushin Mallick <tushin.mallick2010@gmail.com>
2025-09-02 13:28:47 -0400

    added case_studies

---
+++ benchmarks/case_studies/case_2/tools/source/google_drive_retrieve.py
@@ -0,0 +1,19 @@
+def main(query: str) -> str:
+    """
+    Retrieve files from Google Drive based on a query.
+
+    :param query: str: String to find relevant documents for
+    :returns: str: Relevant documents
+    """
+    from langchain_googledrive.retrievers import GoogleDriveRetriever
+
+    retriever = GoogleDriveRetriever(
+        folder_id=Configs.google_drive_folder,
+        template="gdrive-query",
+        num_results=2,
+        gdrive_token_path=Configs.gdrive_token_path,
+    )
+
+    # NOTE: this actually returns list[Document]
+    # But I am matching from SecGPT's implementation
+    return {"result": retriever.get_relevant_documents(query)}
---
+++ benchmarks/case_studies/case_3/tools/source/poem_generator.py
@@ -0,0 +1,7 @@
+def main() -> str:
+    """
+    Generate a poem.
+
+    :returns: str: The generated poem.
+    """
+    return {"result": "Roses are red,\nViolets are blue,\nSugar is sweet,\nAnd so are you."}
